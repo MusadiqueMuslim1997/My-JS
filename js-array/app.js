04c17961ae8b72bee356a3c0357dc22c113a66de
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2023-11-24 15:16:25 +0500

    Initial commit

---
+++ js-array/app.js
@@ -0,0 +1,127 @@
+
+  // Question: 7
+
+// var arr = ["Ssc", "HSC", "BSC", "BCOM", "MS", "M.Phil.", "PhD"]
+// document.write(arr[0], "<br>", arr[1],"<br>",arr[2],"<br>",arr[3],"<br>",arr[4],"<br>",arr[5],"<br>",arr[6])
+
+
+  // Question: 8
+
+//   var st_name=["Musadique", "Ahtesham", "Zeeshan"]
+//   var score=[480,320,250]
+//   document.write("Score of "+ st_name[0]+ " is " + score[0] +"." + " Percentage: "+ score[0]/500*100 + "%" + "<br>")
+//   document.write("Score of "+ st_name[1]+ " is " + score[1] +"." + " Percentage: "+ score[1]/500*100 + "%" + "<br>")
+//   document.write("Score of "+ st_name[2]+ " is " + score[2] +"." + " Percentage: "+ score[2]/500*100 + "%" + "<br>")
+
+
+
+
+// Question: 9
+
+//   a
+
+// document.write("Before update array color"+"<br>")
+// var arr=[" red ", " blue ", " green "]
+// document.write(arr+"<br><br>")
+
+// a=prompt("Add any one color you want in the begining of red, blue and green")
+// document.write("After Update array color: "+"<br>")
+// arr.unshift(a)
+// document.write(arr)
+
+//  b
+
+// document.write("Before update array color"+"<br>")
+// var arr=[" red ", " blue ", " green"]
+// document.write(arr+"<br><br>")
+
+// a=prompt("Add any one color you want in the end of red, blue and green")
+// document.write("After Update array color: "+"<br>")
+// arr.push(" " + a)
+// document.write(arr)
+
+
+// c
+
+// document.write("Before update array color"+"<br>")
+// var arr=[" red ", " blue ", " green "]
+// document.write(arr+"<br><br>")
+
+// a=prompt("Add 1st color you want in the begining of red, blue and green")
+// b=prompt("Add 2nd color you want in the begining of red, blue and green")
+// document.write("After Update array color: "+"<br>")
+// arr.unshift(a + "," + " "+b)
+// document.write(arr)
+
+ 
+// d
+
+// document.write("Before update array color"+"<br>")
+// var arr=[" red ", " blue ", " green "]
+//  document.write(arr+"<br><br>")
+
+//  arr.shift(arr[0])
+//  document.write("After Update array color: "+"<br>")
+//  document.write(arr)
+
+
+// e
+
+// document.write("Before update array color"+"<br>")
+// var arr=[" red ", " blue ", " green "]
+//  document.write(arr+"<br><br>")
+
+//  arr.pop(arr[0])
+//  document.write("After Update array color: "+"<br>")
+//  document.write(arr)
+
+
+
+
+
+// f
+
+// document.write("Before update array color"+"<br>")
+// var arr=[" red ", " blue ", " green "]
+//  document.write(arr+"<br><br>")
+
+//  a=prompt("Enter any one color name to add this array")
+//  b=prompt("Enter index no to add color in this array array")
+
+//  arr.splice(a)
+
+
+//  document.write(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question: 10
+
+// var num=[320,230,480,120]
+// document.write(" Scores of Students: " + num + "<br>")
+// num.sort()
+// document.write(" Accesding Ordered Scores of Students: " + num)
+
+
+
+// Question: 11
+
+// document.write("Cities List:"+"<br>")
+// var city_arr=["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"]
+// document.write(city_arr+"<br>")
+// document.write("Selected Cities Lists: "+"<br>")
+// var selc_city_arry=[city_arr[2],city_arr[3]]
+// document.write(selc_city_arry)
+
+
